1ae8a3bc6645c31f92f720018d3cb78886779700
Marvin Heimbrodt <marvin@6uhrmittag.de>
2019-01-25 21:07:00 +0100

    fix test after rename

--- tests/test_admindojo.py
+++ tests/test_admindojo.py
@@ -33,6 +33,6 @@ def test_command_line_interface():
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert 'admindojo.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(cli.cli, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
