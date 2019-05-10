d5ad1d4ce133f391279bd9cb5d9ed910305d029f
Marvin Heimbrodt <marvin@6uhrmittag.de>
2019-05-10 21:50:53 +0200

    Bump version: 0.1.24 → 0.1.25

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/admindojo/admindojo-client',
-    version='0.1.24',
+    version='0.1.25',
     zip_safe=False,
 )
